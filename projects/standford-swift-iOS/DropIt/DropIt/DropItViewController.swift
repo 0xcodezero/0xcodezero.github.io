89605d649eb7cef4e820ebfa7414a1e780eefe4b
Ahmed Ghalab <0xcodezero@gmail.com>
2016-02-29 15:00:31 +0300

    feature: First usage of extentions, randoms

---
+++ projects/standford-swift-iOS/DropIt/DropIt/DropItViewController.swift
@@ -0,0 +1,65 @@
+//
+//  DropItViewController.swift
+//  DropIt
+//
+//  Created by Ahmed Ghalab on 2/29/16.
+//  Copyright © 2016 Codivity, LLC. All rights reserved.
+//
+
+import UIKit
+
+class DropItViewController: UIViewController {
+
+    @IBOutlet weak var gameView: UIView!
+    
+    var dropsPerRow = 10
+    
+    var dropSize : CGSize{
+        let borderLength = self.view.frame.size.width / CGFloat(dropsPerRow)
+        return CGSizeMake(borderLength, borderLength)
+    }
+    
+    @IBAction func pan(sender: UITapGestureRecognizer) {
+        pan()
+    }
+    
+    private func pan()
+    {
+        var frame =  CGRect(origin: CGPointZero, size: dropSize)
+        frame.origin.x = CGFloat.random(dropsPerRow) * dropSize.width
+        
+        let dropView = UIView(frame: frame)
+        dropView.backgroundColor = UIColor.random()
+        
+        gameView.addSubview(dropView)
+    }
+    
+}
+
+private extension CGFloat
+{
+    static func random(max: Int) -> CGFloat
+    {
+        return CGFloat(arc4random() % UInt32(max))
+    }
+}
+
+
+private extension UIColor
+{
+    static func random() -> UIColor
+    {
+        let number = arc4random() % UInt32(5)
+        switch number
+        {
+            case 0: return UIColor.redColor()
+            case 1: return UIColor.greenColor()
+            case 2: return UIColor.yellowColor()
+            case 3: return UIColor.blackColor()
+            case 4: return UIColor.purpleColor()
+        default:
+            return UIColor.blackColor()
+        }
+        
+    }
+}

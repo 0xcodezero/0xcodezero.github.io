f0bccb690011d427e187bae083dbfc5321c2b847
Ahmed Ghalab <0xcodezero@gmail.com>
2016-03-24 14:39:19 +0300

    Project: Cpp Demo to connect with node.js  project

---
+++ gistic/haram/Nodejs-C++Demo/Nodejs-C++Demo/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Nodejs-C++Demo
+//
+//  Created by Ahmed Ghalab on 3/23/16.
+//  Copyright © 2016 GIS Technology Innovation Center. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
